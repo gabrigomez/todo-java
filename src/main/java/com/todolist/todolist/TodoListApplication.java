a2117b0ed7039f43fec0e9a16452985d55c792d4
Gabriel <gabrigomez15@gmail.com>
2023-07-27 14:58:28 -0300

    fix: use .env in db connection

--- src/main/java/com/todolist/todolist/TodoListApplication.java
+++ src/main/java/com/todolist/todolist/TodoListApplication.java
@@ -2,7 +2,11 @@ package com.todolist.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration
+@PropertySource("file:${user.dir}/.env")
 @SpringBootApplication
 public class TodoListApplication {
 
